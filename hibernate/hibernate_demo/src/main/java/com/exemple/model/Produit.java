efdf9d79c2bfce6b0641f8eda12f728df580b3f7
nessaiba2003-ui <nessaiba2003@gmail.com>
2026-02-18 01:07:03 +0000

    Add files via upload

---
+++ hibernate/hibernate_demo/src/main/java/com/exemple/model/Produit.java
@@ -0,0 +1,62 @@
+package com.example.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+public class Produit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+    private BigDecimal prix;
+
+    // C'est un Constructeur par défaut requis par JPA
+    public Produit() {
+    }
+
+    public Produit(String nom, BigDecimal prix) {
+        this.nom = nom;
+        this.prix = prix;
+    }
+
+    // Ici les Getters et Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public BigDecimal getPrix() {
+        return prix;
+    }
+
+    public void setPrix(BigDecimal prix) {
+        this.prix = prix;
+    }
+
+//methode
+    @Override
+    public String toString() {
+        return "Produit{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", prix=" + prix +
+                '}';
+    }
+}
